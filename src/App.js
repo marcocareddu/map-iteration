631ffaac3be8cba7695bc007c170b127c40f51a8
marcocareddu <saldumaccu@tuta.io>
2024-01-10 09:53:34 +0100

    Added key & moved data in cards.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import Card from "./Card";
-
-const cards = [{
-  username: "Marco",
-  price: 2.50,
-  url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b13305_1.jpg",
-  title: "200€ Airbnb +400P"
-},
-{
-  username: "Lucia",
-  price: 5.50,
-  url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b17471_1.jpg",
-  title: "20€ PS Store +40P"
-}]
+import cards from "./cards";
 
 function App() {
   return (
@@ -20,7 +8,7 @@ function App() {
 
         {/* Use of array.map */}
         {cards.map(( singleCard => {
-          return <Card {...singleCard} />
+          return <Card key={singleCard.id} {...singleCard} />
         }))}
 
       </div>
---
+++ src/cards.js
@@ -0,0 +1,16 @@
+const cards = [{
+    id: 1,
+    username: "Marco",
+    price: 2.50,
+    url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b13305_1.jpg",
+    title: "200€ Airbnb +400P"
+  },
+  {
+    id: 2,
+    username: "Lucia",
+    price: 5.50,
+    url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b17471_1.jpg",
+    title: "20€ PS Store +40P"
+  }]
+
+  export default cards
